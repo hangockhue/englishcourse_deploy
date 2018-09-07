5bc5921585e62873847fbb0f54dd14fff56969d0
hangockhue <healwayhappy@gmail.com>
2018-09-08 00:17:24 +0700

    update page

--- englishcenter/urls.py
+++ englishcenter/urls.py
@@ -7,7 +7,10 @@ from . import views
 app_name = 'englishcourse'
 
 urlpatterns = [
-    path('', views.index, name='index'),
+    path('',views.index, name='index'),
     path('course', views.course, name='course'),
-    path('register', views.register, name='register')
+    path('register', views.register, name='register'),
+    path('header', views.header, name='header'),
+    path('about', views.about, name='about'),
+    path('approach', views.approach, name='approach')
 ]
--- englishcenter/views.py
+++ englishcenter/views.py
@@ -6,14 +6,14 @@ from .forms import StudentForm
 from django.http import Http404
 from django.views.generic import TemplateView
 from django.shortcuts import render, get_object_or_404
-from .models import Student, Teacher, Course, InformationIndex
+from .models import Student, Teacher, Course, InformationIndex, InformationCenter
 # Create your views here.
 
 
 def index(request):
 
     index_information = get_object_or_404(InformationIndex)
-
+    center_information = InformationCenter.objects.get(pk=1)
     if request.method == "POST":
         try:
             student_name = request.POST['student_name']
@@ -26,22 +26,29 @@ def index(request):
             student_form = Student(name=student_name, email=email, number_phone=numberphone,
                                    address=address, sex=sex, date_birth=datebirth)
             student_form.save()
-            return render(request, 'englishcenter/index.html.html')
+            return render(request, 'englishcenter/index.html')
 
         except Exception as e:
             raise Http404("Lỗi nhập form đăng ký")
 
-    return render(request, 'englishcenter/index.html', {'index_information': index_information})
+    return render(request, 'englishcenter/index.html', {'index_information': index_information},
+                  {'center_information': center_information})
 
 
 def course(request):
     course_data = get_object_or_404(Course)
     return render(request, 'englishcenter/course.html', {'course_data': course_data})
 
+def about(request):
+    return render(request, 'englishcenter/about.html')
 
-def index_information(request):
-    return True
+def approach(request):
+    return render(request, 'englishcenter/approach.html')
 
+
+def header(request):
+    center_information = InformationCenter.objects.get(pk=1)
+    return render(request, 'englishcenter/header.html', {'center_information': center_information})
 
 def register(request):
     if request.method == 'POST':
